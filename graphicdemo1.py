5425dca8e643fecde104ddce73501d85492a3281
BIGm0 <34241387+BIGm0@users.noreply.github.com>
2018-01-22 15:28:09 +0000

    Update and rename graficdemo1.py to graphicdemo1.py

--- graphicdemo1.py
+++ graphicdemo1.py
@@ -29,46 +29,15 @@ class PygView(object):
         self.fps = fps
         self.playtime = 0.0
         self.font = pygame.font.SysFont('mono', 24, bold=True)
-
+        self.allgroup =pygame.sprite.LayeredUpdates()
+        self.ballgroup=pygame.sprite.Group()
+        
+        Ball.groups=self.allgroup,self.ballgroup
+        
+        
     def paint(self):
         """painting on the surface"""
-        #pygame.draw.line(Surface, color, start, end, width=1)
-        #pygame.draw.rect(self.background, (0,0,255), (0,0,725,420)) # rect: (x1, y1, width, height)
-        #pygame.draw.rect(self.background, (0,0,0), (725,0,725,420))
-        #pygame.draw.rect(self.background, (0,0,0), (0,420,725,420))
-        #pygame.draw.rect(self.background, (0,0,255), (720,420,725,420))
-        #pygame.draw.line(self.background, (1,0,128), (0,0), (1450,840),(20))
-        #pygame.draw.line(self.background, (1,0,128), (1450,0), (0,840),(20))
-        #pygame.draw.line(self.background, (128,0,1), (0,10), (1450,10),(10))
-        #pygame.draw.line(self.background, (128,0,1), (1450/2,0), (1450/2,840),(10))
-        #pygame.draw.rect(Surface, color, Rect, width=0): return Rect
-        #pygame.draw.rect(self.background, (0,0,255), (0,0,725,420)) # rect: (x1, y1, width, height)
-        #pygame.draw.rect(self.background, (0,0,0), (725,0,725,420))
-        #pygame.draw.rect(self.background, (0,0,0), (0,420,725,420))
-        #pygame.draw.rect(self.background, (0,0,255), (720,420,725,420))
-        #pygame.draw.circle(Surface, color, pos, radius, width=0): return Rect
-        #pygame.draw.circle(self.background, (0,200,0), (0,0), 300)
-        #pygame.draw.circle(self.background, (0,200,0), (1450,0), 300)
-        #pygame.draw.circle(self.background, (0,200,0), (1450,840), 300)
-        #pygame.draw.circle(self.background, (0,200,0), (0,840), 300)
-        #pygame.draw.circle(self.background, (0,200,0), (725,420), 200)
-        #pygame.draw.lines(self.background, (64,64,64), False,
-        #[(0,350), (15,300),(30,400), (45,250),(70,250),(90,300),(100,500),(150,700)])
-        #pygame.draw.ellipse(self.background, (255,0,0), (0,0,1450,815))
-        #pygame.draw.polygon(Surface, color, pointlist, width=0): return Rect
-        #pygame.draw.polygon(self.background, (0,180,0), ((250,100),(300,0),(350,50)))
-        #pygame.draw.arc(Surface, color, Rect, start_angle, stop_angle, width=1): return Rect
-        #pygame.draw.arc(self.background, (0,150,0),(400,10,150,100), 0, 3.14) # radiant instead of grad
-        #pygame.draw.rect(self.background, (0,0,255), (0,0,725,420),1)
-        #pygame.draw.ellipse(self.background, (255,0,0), (0,0,725,420))
-        #pygame.draw.circle(self.background, (0,200,0), (365,210), 200)
-        #pygame.draw.ellipse(self.background, (255,0,0), (365,0,50,400))
-        #start = degrees_to_radians(90)
-        #end = degrees_to_radians(5)
-        #for x in range(0,1450,100):
-            #for y in range(0,840,50):
-                #pygame.draw.line(self.background, (0,0,255),(x,0),(0,y))
-                 #pygame.draw.arc(self.background, (0,150,0),(725,10,725,420),start,end,1)
+        #pygame.draw.arc(self.background, (0,150,0),(725,10,725,420),start,end,1)
         xpoints=[]
         ypoints=[]
         xdist=PygView.width//100
@@ -92,7 +61,10 @@ class PygView(object):
             
     def run(self):
         self.paint() 
-        myball = Ball() # creating the Ball object
+        myball = Ball(startx=100,starty=200) # creating the Ball object
+        myball2= Ball( radius = 10, color=(0,255,0), x=500, y=160,
+                    up=pygame.K_UP,down=pygame.K_DOWN,left=pygame.K_LEFT,right=pygame.K_RIGHT,
+                    startx=300,starty=200)
         running = True
         while running:
             milliseconds = self.clock.tick(self.fps)
@@ -108,29 +80,24 @@ class PygView(object):
                     if event.key == pygame.K_ESCAPE:
                         running = False
                     if event.key == pygame.K_q: # stopper 
-                        myball.dx = 0
-                        myball.dy = 0
-                        myball.x = PygView.width //3 # one third without remainder
-                        myball.y = PygView.height // 2 # one half without remainder
-            pressedkeys = pygame.key.get_pressed() # keys that you can press all the time
-            if pressedkeys[pygame.K_a]:
-                myball.dx -=100
-            if pressedkeys[pygame.K_d]:
-                myball.dx +=100
-            if pressedkeys[pygame.K_w]:
-                myball.dy -= 100
-            if pressedkeys[pygame.K_s]:
-                myball.dy += 100
+                        for ball in self.ballgroup:
+                            ball.x=ball.startx
+                            ball.y=ball.starty
+                            ball.dx,ball.dy=0,0
+                            
             pygame.display.flip()
             self.screen.blit(self.background, (0, 0))
-            myball.update(seconds)
-            myball.blit(self.screen) # blitting it
+            self.allgroup.update(seconds)
+            self.allgroup.draw(self.screen)
+            #myball.update(seconds)
+            #myball.blit(self.screen) # blitting it
             #tail
-            if len(myball.tail)>2:
-                for a in range(1,len(myball.tail)):
-                    pygame.draw.line(self.screen,(0,0,150),
-                                     myball.tail[a-1],
-                                     myball.tail[a],10-a//10)
+            for ball in self.ballgroup:
+                if len(ball.tail)>2:
+                    for a in range(1,len(ball.tail)):
+                        pygame.draw.line(self.screen,ball.color,
+                                     ball.tail[a-1],
+                                     ball.tail[a],10-a//10)
         pygame.quit()
 
     def draw_text(self, text):
@@ -139,26 +106,47 @@ class PygView(object):
         surface = self.font.render(text, True, (0, 0, 0))
         self.screen.blit(surface, (50,150))
 
-class Ball(object):
-    """this is not a native pygame sprite but instead a pygame surface"""
-    def __init__(self, radius = 20, color=(0,0,255), x=320, y=240):
+class Ball(pygame.sprite.Sprite):
+    """this is a native pygame sprite but instead a pygame surface"""
+    def __init__(self, radius = 5, color=(0,0,255), x=320, y=240,
+                    up=pygame.K_w,down=pygame.K_s,left=pygame.K_a,right=pygame.K_d,
+                    startx=100,starty=100):
         """create a (black) surface and paint a blue ball on it"""
+        self._layer=1
+        pygame.sprite.Sprite.__init__(self,self.groups)
+        self.upkey=up
+        self.downkey=down
+        self.leftkey=left
+        self.rightkey=right
         self.radius = radius
         self.color = color
+        self.startx=startx
+        self.starty=starty
         self.x = x
         self.y = y
         self.dx = 0
         self.dy = 0
         # create a rectangular surface for the ball 50x50
-        self.surface = pygame.Surface((2*self.radius,2*self.radius))    
-        pygame.draw.circle(self.surface, color, (radius, radius), radius) # draw blue filled circle on ball surface
-        self.surface = self.surface.convert() # for faster blitting. 
+        self.image = pygame.Surface((2*self.radius,2*self.radius))
+        self.image.set_colorkey((0,0,0))    
+        pygame.draw.circle(self.image, color, (radius, radius), radius) # draw blue filled circle on ball surface
+        self.image = self.image.convert_alpha() # for faster blitting.
+        self.rect=self.image.get_rect() 
         # to avoid the black background, make black the transparent color:
         # self.surface.set_colorkey((0,0,0))
         # self.surface = self.surface.convert_alpha() # faster blitting with transparent color
         self.tail=[]
         
     def update(self, seconds):
+        pressedkeys = pygame.key.get_pressed() # keys that you can press all the time
+        if pressedkeys[self.leftkey]:
+                self.dx -=10
+        if pressedkeys[self.rightkey]:
+                self.dx +=10
+        if pressedkeys[self.upkey]:
+                self.dy -= 10
+        if pressedkeys[self.downkey]:
+                self.dy += 10
         self.x += self.dx * seconds
         self.y += self.dy * seconds
         # wrap around screen
@@ -180,12 +168,12 @@ class Ball(object):
             self.dy*=-1
         self.tail.insert(0,(self.x,self.y))
         self.tail=self.tail[:128]
-        
+        self.rect.center=self.x,self.y
             
         
-    def blit(self, ground):
-        """blit the Ball on the background"""
-        ground.blit(self.surface, ( self.x, self.y))
+    #def blit(self, ground):
+        #"""blit the Ball on the background"""
+        #ground.blit(self.surface, ( self.x, self.y))
         
 if __name__ == '__main__':
     PygView(1450,815).run() # call with width of window and fps
